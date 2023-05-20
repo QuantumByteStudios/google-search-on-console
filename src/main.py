8cf78fff20195987265f66e837a692fe930c2dae
Harshit Raheja <quantumbytestudios@gmail.com>
2023-05-21 00:09:02 +0530

    Update

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
-import json
 import subprocess
 import platform
 from duckpy import Client
+import GoogleLogo as gl
 import time
 
 if (platform.system() == 'Windows'):
@@ -10,29 +10,19 @@ else:
     subprocess.call("clear", shell=True)  # nosec B602, B607
 
 
-class colors:
-    HEADER = '\033[95m'
-    BLUE = '\033[94m'
-    CYAN = '\033[96m'
-    RED = '\u001b[31m'
-    GREEN = '\u001b[32m'
-    YELLOW = '\u001b[33m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
-    BOLD = '\033[1m'
+# google_text = f'{gl.colors.BLUE}{gl.g}{gl.colors.ENDC}{gl.colors.RED}{gl.o}{gl.colors.ENDC}{gl.colors.YELLOW}{gl.o}{gl.colors.ENDC}{gl.colors.BLUE}{gl.g}{gl.colors.ENDC}{gl.colors.GREEN}{gl.l}{gl.colors.ENDC}{gl.colors.RED}{gl.e}{gl.colors.ENDC}'
 
-
-google_text = f'{colors.BLUE}G{colors.ENDC}{colors.RED}o{colors.ENDC}{colors.YELLOW}o{colors.ENDC}{colors.BLUE}g{colors.ENDC}{colors.GREEN}l{colors.ENDC}{colors.RED}e{colors.ENDC}'
+google_text = gl.google_text
 
 UP = '\033[1A'
 CLEAR = '\x1b[2K'
 
-print(f'\n\t{google_text}')
-query_search = input(f'\tSearch: {colors.CYAN}')
-{colors.ENDC}
+print(google_text)
+query_search = input(f'\tSearch: {gl.colors.CYAN}')
+{gl.colors.ENDC}
 
 print(UP, end=CLEAR)
-print(f'\t{colors.FAIL}Results for: {colors.CYAN}"{query_search}"{colors.ENDC}')
+print(f'\t{gl.colors.FAIL}Results for: {gl.colors.CYAN}"{query_search}"{gl.colors.ENDC}')
 
 try:
     client = Client()
@@ -45,19 +35,19 @@ try:
 
     if (query_search == ':history'):
         history = open('search.history', 'r')
-        print(f'\t{colors.FAIL}Search history:\n{colors.ENDC}')
-        print(f'{colors.CYAN}')
+        print(f'\t{gl.colors.FAIL}Search history:\n{gl.colors.ENDC}')
+        print(f'{gl.colors.CYAN}')
         index = 1
         for line in history:
             print(f'\t{index}. {line}')
             index += 1
-        print(f'{colors.ENDC}')
+        print(f'{gl.colors.ENDC}')
         history.close()
         garbage = input()
         exit()
     else:
         print(
-            f'\t{colors.FAIL}About {len(results)} search results in {searchTime} seconds.{colors.ENDC}\n')
+            f'\t{gl.colors.FAIL}About {len(results)} search results in {searchTime} seconds.{gl.colors.ENDC}\n')
         history = open('search.history', 'a')
         history.write(f'{query_search}\n')
         history.close()
@@ -68,13 +58,14 @@ try:
             url = results[i].url
             description = results[i].description
             print(
-                f'{colors.CYAN}  {result_index}.{colors.ENDC}\t{colors.GREEN}{title}{colors.ENDC}')
-            print(f'\t{colors.BLUE}{url}{colors.ENDC}')
-            print(f'\t{description[0:100]}{colors.RED}...{colors.ENDC}')
+                f'{gl.colors.CYAN}  {result_index}.{gl.colors.ENDC}\t{gl.colors.GREEN}{title}{gl.colors.ENDC}')
+            print(f'\t{gl.colors.BLUE}{url}{gl.colors.ENDC}')
+            print(
+                f'\t{description[0:100]}{gl.colors.RED}...{gl.colors.ENDC}')
             print('')
         garbage = input()
         exit()
 except Exception as e:
-    print(f'{colors.FAIL}No results available.{colors.ENDC}')
+    print(f'{gl.colors.FAIL}No results available.{gl.colors.ENDC}')
     garbage = input()
     exit()
